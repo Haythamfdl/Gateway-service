1256e3a41daf253b90ef89e57ef27bdeeb0c94fb
Haytham <fdlhaytham@gmail.com>
2020-10-11 23:20:04 +0100

    Ajout des Api Countries et Salat

--- src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -1,14 +1,13 @@
 package org.sid.gatewayservice;
 
-import com.sun.xml.internal.ws.util.xml.CDATA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
+@EnableHystrix
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
